2c14bd4efb5052df85e279594e2d533dd4a0f974
aditya728 <90396219+aditya728@users.noreply.github.com>
2026-06-13 12:26:24 +0530

    Added a eureka-server

---
+++ hirehub-eureka-server/src/main/java/com/charlie/hirehub/eureka_server/HirehubEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.charlie.hirehub.eureka_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HirehubEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HirehubEurekaServerApplication.class, args);
+	}
+
+}
---
+++ hirehub-eureka-server/src/test/java/com/charlie/hirehub/eureka_server/HirehubEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.charlie.hirehub.eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HirehubEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
