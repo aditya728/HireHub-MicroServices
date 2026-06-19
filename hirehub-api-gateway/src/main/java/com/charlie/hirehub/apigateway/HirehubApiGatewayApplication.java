51d21ad7dde9775272d28862b037fa96a489c668
aditya728 <90396219+aditya728@users.noreply.github.com>
2026-06-20 00:53:34 +0530

    Added api-gateway

---
+++ hirehub-api-gateway/src/main/java/com/charlie/hirehub/apigateway/HirehubApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.charlie.hirehub.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HirehubApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HirehubApiGatewayApplication.class, args);
+	}
+
+}
---
+++ hirehub-api-gateway/src/test/java/com/charlie/hirehub/apigateway/HirehubApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.charlie.hirehub.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HirehubApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
