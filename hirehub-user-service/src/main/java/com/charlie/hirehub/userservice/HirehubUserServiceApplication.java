f8daaac6556d617b65f3f25b81394728dccda41a
admaurya <asm71035@gmail.com>
2026-07-04 17:01:20 +0530

    user-service init

---
+++ hirehub-user-service/src/main/java/com/charlie/hirehub/userservice/HirehubUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.charlie.hirehub.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HirehubUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HirehubUserServiceApplication.class, args);
+	}
+
+}
---
+++ hirehub-user-service/src/test/java/com/charlie/hirehub/userservice/HirehubUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.charlie.hirehub.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HirehubUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
